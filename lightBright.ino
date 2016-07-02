5a17fe0c9b33dca2214c319d118d77f3cd921c56
Matisse <dfinkel@dfinkel-mbp114.local>
2016-07-02 01:09:01 -0700

    Up down color selector
    Can now change the current led selected
    
    small bug, currently sets the newly selected led to the previous led’s
    color

--- lightBright.ino
+++ lightBright.ino
@@ -33,9 +33,9 @@ struct led {
   }
 
   led(int p) {
-    red = 100;
-    green = 100;
-    blue = 200;
+    red = 0;
+    green = 0;
+    blue = 0;
     pinRed = p;
     pinGreen = p + 1;
     pinBlue = p + 2;
@@ -43,6 +43,7 @@ struct led {
 };
 
 led leds[] = {led(2), led(5)};
+int ledToEdit;
 
 
 void setup() {
@@ -56,29 +57,17 @@ void setup() {
 }
 
 void loop() {
+  //select the led to change
+  ledToEdit = moveCursor();
   
-  up = digitalRead(upPin);
-  down = digitalRead(downPin);
-  left = digitalRead(leftPin);
-  right = digitalRead(rightPin);
-
-  Serial.print(up);
-  Serial.print("\t");
-  Serial.print(down);
-  Serial.print("\t");
-  Serial.print(left);
-  Serial.print("\t");
-  Serial.println(right);
+  //getting color to set it to
+  //for (int i = 0; i < (sizeof(leds) / sizeof(led)); ++i) {
+    leds[ledToEdit].red = colors[colorChange][0];
+    leds[ledToEdit].green = colors[colorChange][1];
+    leds[ledToEdit].blue = colors[colorChange][2];
+  //}
   
-  
-  //getting colors
-  for (int i = 0; i < (sizeof(leds) / sizeof(led)); ++i) {
-    leds[i].red = colors[colorChange][0];
-    leds[i].green = colors[colorChange][1];
-    leds[i].blue = colors[colorChange][2];
-  }
-  
-  //display
+  //display the led's
   for (int i = 0; i < (sizeof(leds) / sizeof(led)); ++i) {
     analogWrite(leds[i].pinRed, leds[i].red * colorIntensity);
     analogWrite(leds[i].pinGreen, leds[i].green * colorIntensity);
@@ -86,6 +75,8 @@ void loop() {
 
     //debugInfo(rgb1[i]);
   }
+
+  //get the button input for changing to the next color on the current pin
   colorChange = (colorChange + digitalRead(colorPin)) % numColors;
   //Serial.println(colorChange);
 
@@ -97,6 +88,29 @@ void loop() {
   //   trigger input
   //delay(10)
   delay(250);
+}
+
+//move the currently selected led for the light bright
+int moveCursor() {
+  up = digitalRead(upPin);
+  down = digitalRead(downPin);
+  left = digitalRead(leftPin);
+  right = digitalRead(rightPin);
+
+  Serial.print(up);
+  Serial.print("\t");
+  Serial.print(down);
+  Serial.print("\t");
+  Serial.print(left);
+  Serial.print("\t");
+  Serial.print(right);
+
+  ledToEdit = constrain(ledToEdit + down - up, 0, sizeof(leds)/sizeof(led) - 1);
+  
+  Serial.print("\t");
+  Serial.println(ledToEdit);
+  
+  return ledToEdit;
 }
 
 void debugInfo(led curColor) {
