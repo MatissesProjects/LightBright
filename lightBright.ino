4823006b6d4a05cec0ba90d363eeb6ecabc5fa51
Matisse <dfinkel@dfinkel-mbp114.local>
2016-07-02 01:29:40 -0700

    fixed the passing color selector bug
    now the color of the led will stay the same color when selected.

--- lightBright.ino
+++ lightBright.ino
@@ -2,26 +2,28 @@ int colorChange = 0; // 0-r 1-g 2-b
 int colorPin = 12;
 int numColors;
 
-int potPin = A0;
-
+int prevColor;
 int colorIntensity = 15;
 const int colorDim = 3;
-int colors[][colorDim] = {{0, 0, 0},
-                          {1, 0, 0},
-                          {1, 1, 0},
-                          {0, 1, 0},
-                          {0, 1, 1},
-                          {0, 0, 1},
-                          {1, 0, 1},
-                          {1, 1, 1}};
+int colors[][colorDim] = {
+  {0, 0, 0},
+  {1, 0, 0},
+  {1, 1, 0},
+  {0, 1, 0},
+  {0, 1, 1},
+  {0, 0, 1},
+  {1, 0, 1},
+  {1, 1, 1}
+};
 
 int up, down, left, right;
 int upPin = 50, downPin = 53, leftPin = 52, rightPin = 51;
 
 struct led {
   int red, green, blue;
-
   int pinRed, pinBlue, pinGreen;
+
+  int indexOfColor;
 
   led(int r, int g, int b, int pr, int pg, int pb) {
     red = r;
@@ -30,12 +32,16 @@ struct led {
     pinRed = pr;
     pinGreen = pg;
     pinBlue = pb;
+
+    //todo fix
+    indexOfColor = 0;
   }
 
   led(int p) {
     red = 0;
     green = 0;
     blue = 0;
+    indexOfColor = 0;
     pinRed = p;
     pinGreen = p + 1;
     pinBlue = p + 2;
@@ -43,7 +49,7 @@ struct led {
 };
 
 led leds[] = {led(2), led(5)};
-int ledToEdit;
+int ledToEdit = 0;
 
 
 void setup() {
@@ -53,20 +59,27 @@ void setup() {
   pinMode(downPin, INPUT);
   pinMode(leftPin, INPUT);
   pinMode(rightPin, INPUT);
-  numColors = (sizeof(colors)/sizeof(int[colorDim]));
+  numColors = (sizeof(colors) / sizeof(int[colorDim]));
 }
 
 void loop() {
   //select the led to change
-  ledToEdit = moveCursor();
+  bool didMove = moveCursor();
+  if(didMove)
+    colorChange = leds[ledToEdit].indexOfColor;
+    
+  Serial.print("\t");
+  Serial.print(ledToEdit);
+  Serial.print("\t\t");
+  Serial.println(didMove);
   
   //getting color to set it to
-  //for (int i = 0; i < (sizeof(leds) / sizeof(led)); ++i) {
-    leds[ledToEdit].red = colors[colorChange][0];
-    leds[ledToEdit].green = colors[colorChange][1];
-    leds[ledToEdit].blue = colors[colorChange][2];
-  //}
+  leds[ledToEdit].red = colors[colorChange][0];
+  leds[ledToEdit].green = colors[colorChange][1];
+  leds[ledToEdit].blue = colors[colorChange][2];
   
+  leds[ledToEdit].indexOfColor = colorChange;
+
   //display the led's
   for (int i = 0; i < (sizeof(leds) / sizeof(led)); ++i) {
     analogWrite(leds[i].pinRed, leds[i].red * colorIntensity);
@@ -76,8 +89,10 @@ void loop() {
     //debugInfo(rgb1[i]);
   }
 
+  prevColor = colorChange;
   //get the button input for changing to the next color on the current pin
   colorChange = (colorChange + digitalRead(colorPin)) % numColors;
+
   //Serial.println(colorChange);
 
   //we should use LCM of the tasks that we have, so we currently have
@@ -91,7 +106,7 @@ void loop() {
 }
 
 //move the currently selected led for the light bright
-int moveCursor() {
+bool moveCursor() {
   up = digitalRead(upPin);
   down = digitalRead(downPin);
   left = digitalRead(leftPin);
@@ -105,23 +120,21 @@ int moveCursor() {
   Serial.print("\t");
   Serial.print(right);
 
-  ledToEdit = constrain(ledToEdit + down - up, 0, sizeof(leds)/sizeof(led) - 1);
-  
-  Serial.print("\t");
-  Serial.println(ledToEdit);
-  
-  return ledToEdit;
+  int prevLed = ledToEdit;
+  ledToEdit = constrain(ledToEdit + down - up, 0, sizeof(leds) / sizeof(led) - 1);
+
+  return prevLed != ledToEdit;
 }
 
 void debugInfo(led curColor) {
   Serial.print(colorChange);
-    Serial.print("\t");
-    /*
+  Serial.print("\t");
+  /*
     Serial.print(curColor.red);
     Serial.print("\t");
     Serial.print(curColor.green);
     Serial.print("\t");
     Serial.print(curColor.blue);*/
-    Serial.println("\n\n");
+  Serial.println("\n\n");
 }
 
