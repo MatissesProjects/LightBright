a58744693bb21e2bac49979e8d2ebebd70e50d37
Matisse <dfinkel@dfinkel-mbp114.local>
2016-07-03 22:33:22 -0700

    Animations started and cleanup
    basic animations, follower, and a few other fixes, lots of fixes for
    the color - not going into that.
    
    Cleaned more debugging out

--- lightBright.ino
+++ lightBright.ino
@@ -1,30 +1,26 @@
+#define colorred     {1, 0, 0}
+#define colorgreen   {0, 1, 0}
+#define colorblue    {0, 0, 1}
+#define colorcyan    {0, 1, 1}
+#define coloryellow  {1, 1, 0}
+#define colorpurple  {1, 0, 1}
+#define colorwhite   {1, 1, 1}
+#define coloroff     {0, 0, 0}
+
+int colors[8][3] = {
+  coloroff, colorred, coloryellow, colorgreen,
+  colorcyan, colorblue, colorpurple, colorwhite
+};
+
 int colorChange = 0; // 0-r 1-g 2-b
-int colorPin = 22;
+int colorPin = 122;
 int numColors;
 
 int prevColor;
 int colorIntensity = 128;
 const int colorDim = 3;
 
-int red[3] = {1, 0, 0};
-int green[3] = {0, 1, 0};
-int blue[3] = {0, 0, 1};
-int cyan[3] = {0, 1, 1};
-int yellow[3] = {1, 1, 0};
-int purple[3] = {1, 0, 1};
-int white[3] = {1, 1, 1};
-int off[3] = {0, 0, 0};
-
-int colors[3][8] = {
-  *off,
-  *red,
-  *yellow,
-  *green,
-  *cyan,
-  *blue,
-  *purple,
-  *white
-};
+int ledToEditX = 0, ledToEditY = 1;
 
 int up, down, left, right;
 int upPin = 150, downPin = 153, leftPin = 152, rightPin = 151;
@@ -38,23 +34,19 @@ struct led {
   int indexOfColor;
 
   led(int pr, int pg, int pb, int r, int g, int b) {
-    init(r, g, b, pr, pg, pb);
-  }
-
-  led(int p, int r, int g, int b) {
-    init(r, g, b, p, p + 1, p + 2);
+    init(r, g, b, pr, pg, pb, 0);
   }
 
   led(int p, int * c) {
-    init(c[0], c[1], c[2], p, p + 1, p + 2);
+    init(c[0], c[1], c[2], p, p + 1, p + 2, 0);
   }
 
   //debug
   led(int p) {
-    init(0, 1, 0, p, p + 1, p + 2);
+    init(0, 1, 0, p, p + 1, p + 2, 0);
   }
 
-  void init(int r, int g, int b, int pr, int pg, int pb) {
+  void init(int r, int g, int b, int pr, int pg, int pb, int iofColor) {
     red = r;
     green = g;
     blue = b;
@@ -63,25 +55,27 @@ struct led {
     pinBlue = pb;
 
     //todo fix
-    indexOfColor = 0;
+    indexOfColor = iofColor;
 
     pinMode(pinRed, OUTPUT);
     pinMode(pinBlue, OUTPUT);
     pinMode(pinGreen, OUTPUT);
+  }
+
+  void setColor(int * color) {
+    red = color[0];
+    green = color[1];
+    blue = color[2];
   }
 };
 
 // keep these synced with the led matrix below.
 const int matrixHeight = 3, matrixWidth = 3;
 led leds[][matrixHeight] = {
-  {led(22, red), led(25, blue), led(28, green)},
-  {led(31, purple), led(34, white), led(37, blue)},
-  {led(40, yellow), led(43, cyan), led(46, green)}
-};// matrix is transposed on board
-// 2 8
-// 5 11
-
-int ledToEditX = 0, ledToEditY = 0;
+  {led(22), led(25), led(28)},
+  {led(31), led(34), led(37)},
+  {led(40), led(43), led(46)}
+};
 
 void setup() {
   Serial.begin(9600);
@@ -92,7 +86,7 @@ void setup() {
   pinMode(rightPin, INPUT);
   numColors = (sizeof(colors) / sizeof(int[colorDim]));
   delay(50);
-  blinkRGBLed(leds[0][0], blinkTimeMS);
+  blinkRGBLed(leds[ledToEditX][ledToEditY], blinkTimeMS);
 }
 
 void loop() {
@@ -102,33 +96,25 @@ void loop() {
     blinkRGBLed(leds[ledToEditX][ledToEditY], blinkTimeMS);
   }
 
-  /*Serial.print("(");
-    Serial.print(ledToEditX);
-    Serial.print(", ");
-    Serial.print(ledToEditY);
-    Serial.print(")\t");
-    Serial.print(leds[ledToEditX][ledToEditY].indexOfColor);
-    Serial.println();
-  */
-  //getting color to set it to
-  leds[ledToEditX][ledToEditY].red = colors[colorChange][0];
-  leds[ledToEditX][ledToEditY].green = colors[colorChange][1];
-  leds[ledToEditX][ledToEditY].blue = colors[colorChange][2];
+  //debugPrintLed();
 
+  //getting color to set it to
+  leds[ledToEditX][ledToEditY].setColor(colors[colorChange]);
   leds[ledToEditX][ledToEditY].indexOfColor = colorChange;
 
   //display the led's
   for (int x = 0; x < matrixWidth; ++x) {
     for (int y = 0; y < matrixHeight; ++y) {
+      prevColor = leds[x][y].indexOfColor;//colorChange;
+      colorChange++;
+      //get the button input for changing to the next color on the current pin
+      colorChange = (colorChange + digitalRead(colorPin)) % numColors;
+
+      leds[x][y].setColor(colors[colorChange]);
+
       paintRGBLed(leds[x][y]);
-      //debugInfo(leds[x][y]);
     }
   }
-
-  prevColor = colorChange;
-  //get the button input for changing to the next color on the current pin
-  colorChange = (colorChange + digitalRead(colorPin)) % numColors;
-
   //Serial.println(colorChange);
 
   //we should use LCM of the tasks that we have, so we currently have
@@ -176,6 +162,17 @@ void blinkRGBLed(led l, int timeToBlink) {
   }
   delay(timeToBlink);
   paintRGBLed(l);
+}
+
+void debugPrintLed() {
+  Serial.print("(");
+  Serial.print(ledToEditX);
+  Serial.print(", ");
+  Serial.print(ledToEditY);
+  Serial.print(")\t");
+  Serial.print(leds[ledToEditX][ledToEditY].indexOfColor);
+  Serial.println();
+
 }
 
 void debugDirection() {
