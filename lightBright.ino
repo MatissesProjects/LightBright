925cf83a88b6466757a25584554d435bd0d2ab01
Matisse <dfinkel@dfinkel-mbp114.local>
2016-09-21 00:24:05 -0700

    Code cleanup and project communication
    Now we can use the remote to change the LED color that we are currently
    on
    led represented by the number pressed on the controller as follows
    (controller -> led)
    1 -> 1    2 -> 2
    4 -> 3   5 -> 4

--- lightBright.ino
+++ lightBright.ino
@@ -5,17 +5,9 @@ boolean transmitDataComplete = false;
 int irInput = 0;
 
 int colorChange = 0; // 0-r 1-g 2-b
-int colorPin = 122;
 int numColors;
 
 const int colorDim = 3;
-
-int ledToEditX = 0, ledToEditY = 1;
-
-int up, down, left, right;
-int upPin = 150, downPin = 153, leftPin = 152, rightPin = 151;
-
-int blinkTimeInitial = 50, ledBlinkSpeed = 500;
 
 // keep these synced with the led matrix below.
 const int matrixHeight = 2, matrixWidth = 2;
@@ -27,45 +19,45 @@ led leds[][matrixHeight] = {
 
 void setup() {
   Serial.begin(9600);
-  /*pinMode(colorPin, INPUT);
-  pinMode(upPin, INPUT);
-  pinMode(downPin, INPUT);
-  pinMode(leftPin, INPUT);
-  pinMode(rightPin, INPUT);
-  */
   numColors = (sizeof(colors) / sizeof(int[colorDim]));
-  delay(blinkTimeInitial);
-  leds[ledToEditX][ledToEditY].blinkRGBLed(blinkTimeInitial);
 }
 
 void loop() {
   if (transmitDataComplete) {
     transmitDataComplete = false;
     Serial.println(irInput);
+    int xInput, yInput;
+    switch(irInput) {
+      case 1:
+        xInput = 0;
+        yInput = 0;
+        break;
+      case 2:
+        xInput = 1;
+        yInput = 0;
+        break;
+      case 4:
+        xInput = 0;
+        yInput = 1;
+        break;
+      case 5:
+        xInput = 1;
+        yInput = 1;
+        break;
+    }
+  
+  leds[xInput][yInput].setColor(colors[leds[xInput][yInput].indexOfColor++]);
+  leds[xInput][yInput].indexOfColor %= 8;
   }
   
-  //select the led to change
-  //if (moveCursor()) {
-  //colorChange = leds[ledToEditX][ledToEditY].indexOfColor;
-  //  leds[ledToEditX][ledToEditY].blinkRGBLed(blinkTimeMS);
-  //}
-
-  //debugPrintLed();
-
-  //getting color to set it to
-  //leds[ledToEditX][ledToEditY].setColor(colors[colorChange]);
-  //leds[ledToEditX][ledToEditY].indexOfColor = colorChange;
-
   //display the led's
   for (int x = 0; x < matrixWidth; ++x) {
     for (int y = 0; y < matrixHeight; ++y) {
-
       //colorChange++;
       //get the button input for changing to the next color on the current pin
       //colorChange = (colorChange + digitalRead(colorPin)) % numColors;
 
-      leds[x][y].setColor(animation[frame][x * matrixWidth + y]);
-
+      
       leds[x][y].paintRGBLed();
     }
   }
@@ -78,7 +70,6 @@ void loop() {
   //    trigger display
   //   trigger input
   //delay(10)
-  delay(ledBlinkSpeed);
 }
 
 void serialEvent() {
@@ -93,40 +84,4 @@ void serialEvent() {
   }
 }
 
-//move the currently selected led for the light bright
-bool moveCursor() {
-  up = digitalRead(upPin);
-  down = digitalRead(downPin);
-  left = digitalRead(leftPin);
-  right = digitalRead(rightPin);
 
-  //debugDirection();
-
-  int prevLedX = ledToEditX;
-  int prevLedY = ledToEditY;
-  ledToEditY = constrain(ledToEditY + down - up, 0, matrixHeight - 1);
-  ledToEditX = constrain(ledToEditX + right - left, 0, matrixWidth - 1);
-
-  return prevLedX != ledToEditX || prevLedY != ledToEditY;
-}
-
-void debugPrintLed() {
-  Serial.print("(");
-  Serial.print(ledToEditX);
-  Serial.print(", ");
-  Serial.print(ledToEditY);
-  Serial.print(")\t");
-  Serial.print(leds[ledToEditX][ledToEditY].indexOfColor);
-  Serial.println();
-
-}
-
-void debugDirection() {
-  Serial.print(up);
-  Serial.print("\t");
-  Serial.print(down);
-  Serial.print("\t");
-  Serial.print(left);
-  Serial.print("\t");
-  Serial.print(right);
-}
